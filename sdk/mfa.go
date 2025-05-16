12deb61f5e3406ca82a7d60eab6e9ae10ec6c5f1
jeseeqing <jeseeqing@gmail.com>
2025-05-16 17:23:21 +0800

    feat: 增加 MfaSetupInitiate 函数用来初始化mfa参数

---
+++ sdk/mfa.go
@@ -0,0 +1,29 @@
+package sdk
+
+import (
+	"encoding/json"
+
+	"github.com/casdoor/casdoor-go-sdk/casdoorsdk"
+)
+
+type MfaSetupInitiateForm struct {
+	Owner string `form:"owner" json:"owner"`
+	// user name
+	Name    string `form:"name" json:"name"`
+	MfaType string `form:"mfaType" json:"mfaType"`
+}
+
+// setup MFA
+func (x *ClientX) MfaSetupInitiate(f *MfaSetupInitiateForm) (*casdoorsdk.Response, error) {
+	bytes, err := json.Marshal(f)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := x.Client.DoPost("/mfa/setup/initiate", nil, bytes, true, false)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
