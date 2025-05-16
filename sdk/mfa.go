7481e896308b5587c7eedb8705dd6b7e501add92
jeseeqing <jeseeqing@gmail.com>
2025-05-16 17:45:21 +0800

    feat: add MfaSetupVerify func that verify mfa setup

--- sdk/mfa.go
+++ sdk/mfa.go
@@ -27,3 +27,26 @@ func (x *ClientX) MfaSetupInitiate(f *MfaSetupInitiateForm) (*casdoorsdk.Respons
 
 	return resp, nil
 }
+
+type MfaSetupVerifyForm struct {
+	MfaType     string `form:"mfaType" json:"mfaType"`
+	Passcode    string `form:"passcode" json:"passcode"`
+	Secret      string `form:"secret" json:"secret"`
+	Dest        string `form:"dest" json:"dest"`
+	CountryCode string `form:"countryCode" json:"countryCode"`
+}
+
+// setup MFA verify
+func (x *ClientX) MfaSetupVerify(f *MfaSetupVerifyForm) (*casdoorsdk.Response, error) {
+	bytes, err := json.Marshal(f)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := x.Client.DoPost("/mfa/setup/verify", nil, bytes, true, false)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
